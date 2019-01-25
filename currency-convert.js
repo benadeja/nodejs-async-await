b1c2563cf0b393613d8a8f4007faa020d21e1638
jbenade <jaco.benade@momentum.co.za>
2019-01-25 10:54:30 +0200

    first commit

---
+++ currency-convert.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=f84e933d907f0477009e98ff21339ca1');
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+    return rate;
+  } catch (e) {
+    throw new Error(`Unable to get exhange rate for ${from} and ${to}.`)
+  }
+};
+
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    return response.data.map((country) => country.name);
+  } catch (e) {
+    throw new Error(`Unable to get countires that use ${currencyCode}`);
+  }
+};
+
+const convertCurrency = async (from, to, amount) => {
+  const rate = await getExchangeRate(from, to);
+  const countries = await getCountries(to);
+  const convertedAmount = (amount * rate).toFixed(2);
+  return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(', ')}`;
+};
+
+convertCurrency('USD', 'CAD', 20).then((message) => {
+  console.log(message);
+}).catch((e) => {
+  console.log(e.message);
+});
